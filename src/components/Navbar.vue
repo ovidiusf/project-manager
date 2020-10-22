1e5d0d308e977687f0da8dcaa1f597818f755fdc
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-10-22 11:46:43 -0700

    Add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav>
+    <v-toolbar flat app>
+    <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Project</span>
+        <span>Manager</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat color="grey">
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer v-model="drawer" app bottom class="primary" temporary>
+      <p class="info">test</p>
+    </v-navigation-drawer>
+
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false
+    };
+  }
+};
+</script>
+
+<style></style>
