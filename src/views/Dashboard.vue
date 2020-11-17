6857de9dab2189860a0c8dddf36c6e105d5a159b
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-11-17 11:55:07 -0800

    Add grid and some dummy data

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,20 +2,32 @@
   <div class="home">
     <h1 class="text-h4 grey--text">Dashboard</h1>
     <v-container class="my-5">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab porro maxime
-        ea quo corrupti exercitationem officiis nostrum? Rerum optio inventore
-        debitis magni repellat magnam sapiente labore ipsum dicta amet. Placeat!
-        lit. Ab porro maxime ea quo corrupti exercitationem officiis nostrum?
-        Rerum optio inventore debitis m lit. Ab porro maxime ea quo corrupti
-        exercitationem officiis nostrum? Rerum optio inventore debitis m
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi soluta
-        est voluptate placeat cupiditate eveniet iure magni iusto eos.
-        Explicabo, consequatur! Ipsam distinctio sunt iusto eligendi sequi eius.
-        Nemo, excepturi.
-      </p>
+      <v-card
+        color="blue-grey lighten-1"
+        flat
+        v-for="project in projects"
+        :key="project.title"
+      >
+        <v-row :class="`pa-3 project ${project.status}`" wrap dense>
+          <v-col cols="12" md="6">
+            <div class="text-h5 white--text">Project title</div>
+            <div>{{ project.title }}</div>
+          </v-col>
+          <v-col cols="6" sm="4" md="2">
+            <div class="text-h5 white--text">Person</div>
+            <div>{{ project.person }}</div>
+          </v-col>
+          <v-col cols="6" sm="4" md="2">
+            <div class="text-h5 white--text">Due by</div>
+            <div>{{ project.due }}</div>
+          </v-col>
+          <v-col cols="6" sm="4" md="2">
+            <div class="text-h5 white--text">Status</div>
+            <div>{{ project.status }}</div>
+          </v-col>
+        </v-row>
+        <v-divider></v-divider>
+      </v-card>
     </v-container>
   </div>
 </template>
@@ -23,6 +35,56 @@
 <script>
 // @ is an alias to /src
 export default {
-  name: "Dashboard"
+  name: "Dashboard",
+  data() {
+    return {
+      projects: [
+        {
+          title: "Design a new website",
+          person: "John Smithson",
+          due: "1st Jan 2021",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+        {
+          title: "Code up the homepage",
+          person: "Chung Loi",
+          due: "10th Jan 2021",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+        {
+          title: "Design video thumbnails",
+          person: "Bertha",
+          due: "20th Dec 2020",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+        {
+          title: "Create a community forum",
+          person: "Hadouken",
+          due: "20th Oct 2020",
+          status: "overdue",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        }
+      ]
+    };
+  }
 };
 </script>
+
+<style>
+.project.complete {
+  border-left: 4px solid #3cd1c2;
+}
+.project.ongoing {
+  border-left: 4px solid orange;
+}
+.project.overdue {
+  border-left: 4px solid tomato;
+}
+</style>
