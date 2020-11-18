1aa3fc2b4cef902493f1291b0fb0e1a92268be77
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-11-18 10:16:23 -0800

    Add buttons for sorting and sorting function

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,6 +2,17 @@
   <div class="home">
     <h1 class="text-h4 grey--text">Dashboard</h1>
     <v-container class="my-5">
+      <v-row class="mb-3" dense>
+        <v-btn small flat color="grey" outlined @click="sortBy('title')">
+          <v-icon left small color="blue-grey">folder</v-icon>
+          <span class="text-caption text-lowercase">By project name</span>
+        </v-btn>
+        <v-divider vertical></v-divider>
+        <v-btn small flat color="grey" outlined @click="sortBy('person')">
+          <v-icon left small color="blue-grey">person</v-icon>
+          <span class="text-caption text-lowercase">By person name</span>
+        </v-btn>
+      </v-row>
       <v-card
         color="blue-grey lighten-1"
         flat
@@ -42,6 +53,12 @@
 </template>
 
 <script>
+const statusColors = {
+  complete: "#3cd1c2",
+  ongoing: "orange",
+  overdue: "#FF8A80",
+  default: "indigo"
+};
 // @ is an alias to /src
 export default {
   name: "Dashboard",
@@ -80,21 +97,24 @@ export default {
           content:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
         }
-      ]
+      ],
+      statusColors: statusColors
     };
   },
   methods: {
+    /**
+     * Get the chip color based on the current status
+     */
     chipColor(status) {
-      const statusColors = {
-        complete: "#3cd1c2",
-        ongoing: "orange",
-        overdue: "#FF8A80",
-        default: "indigo"
-      };
-
-      return status in statusColors
-        ? statusColors[status]
-        : statusColors.default;
+      return status in this.statusColors
+        ? this.statusColors[status]
+        : this.statusColors.default;
+    },
+    /**
+     * Sorting function for the array
+     */
+    sortBy(property) {
+      this.projects.sort((a, b) => (a[property] < b[property] ? -1 : 1));
     }
   }
 };
